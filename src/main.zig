e4b8061d6bbd7f203566dbb86326850f7b08b143
pithlessly <79002325+pithlessly@users.noreply.github.com>
2021-12-29 01:49:18 -0800

    basic support for QOI image format
    This implements a converter from QOI to PPM (the alpha channel is simply
    discarded). The formatter still has bugs; although the resulting images
    are recognizable, there are visual artifacts. I probably just have a
    sign wrong somewhere.

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,111 @@
 const std = @import("std");
+const add = std.math.add;
+const mul = std.math.mul;
+const Allocator = std.mem.Allocator;
 
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+const qoi = @import("qoi.zig");
+
+fn encodeDigits(
+    number: u32,
+    out_last_digit: [*]u8,
+) void {
+    var n = number;
+    var p = out_last_digit;
+    while (true) {
+        p[0] = '0' + @intCast(u8, n % 10);
+        p -= 1;
+        n /= 10;
+        if (n == 0) break;
+    }
 }
 
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+fn removeAlpha(px_data: []u8) void {
+    std.debug.assert(px_data.len % 4 == 0);
+    var p1 = px_data.ptr;
+    var p2 = px_data.ptr;
+    var end = px_data.ptr + px_data.len;
+    while (p1 != end) {
+        p2[0..4].* = p1[0..4].*;
+        p1 += 4;
+        p2 += 3;
+    }
+}
+
+fn decodeQoi(
+    alloc: Allocator,
+    input: []align(4) const u8,
+) ![]u8 {
+    const metadata = try qoi.Metadata.compute(input);
+
+    // decode QOI and rewrite to PPM.
+    // we place enough padding in the PPM header to ensure that:
+    // - the pixel data is 4-byte aligned;
+    // - the width and height can be any 32-bit values (which is a little excessive)
+    const header_size = 32;
+    const header_words = @divExact(header_size, 4);
+    const res = try alloc.alloc(u32, header_words + metadata.pixels);
+
+    const header = std.mem.sliceAsBytes(res[0..header_words]);
+    std.mem.copy(u8, header, "P6\n" ++ " " ** (3 + 10 + 1 + 10 + 1) ++ "255\n");
+    encodeDigits(metadata.width, header.ptr + 15);
+    encodeDigits(metadata.height, header.ptr + 26);
+    try qoi.decode(input, res[header_words..]);
+
+    var res_bytes = std.mem.sliceAsBytes(res);
+    removeAlpha(res_bytes[header_size..]);
+    res_bytes.len -= metadata.pixels;
+    return res_bytes;
+}
+
+const in_file_max_size = 1024 * 1024 * 1024; // 1 GiB
+
+fn decode(
+    alloc: Allocator,
+    in_file: []const u8,
+    out_file: []const u8,
+    comptime decodeFn: anytype,
+) !void {
+    const in_data = try std.fs.cwd().readFileAllocOptions(
+        alloc,
+        in_file,
+        in_file_max_size,
+        null,
+        4,
+        null,
+    );
+    const out_data = try decodeFn(alloc, in_data);
+    try std.fs.cwd().writeFile(out_file, out_data);
+}
+
+fn usage(exe_name: []const u8) noreturn {
+    const stderr = std.io.getStdErr().writer();
+    stderr.print(
+        \\Usage: {s} [format] [infile] [outfile]
+        \\Available formats:
+        \\  qoi   Convert QOI to PPM
+        \\
+    , .{exe_name}) catch {};
+    std.process.exit(1);
+}
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+
+    var args = std.process.args();
+    defer args.deinit();
+
+    const exe_name = try args.next(alloc) orelse usage("formatz");
+    const format = try args.next(alloc) orelse usage(exe_name);
+    const in_file = try args.next(alloc) orelse usage(exe_name);
+    const out_file = try args.next(alloc) orelse usage(exe_name);
+
+    if (std.mem.eql(u8, format, "qoi")) {
+        try decode(alloc, in_file, out_file, decodeQoi);
+    } else usage(exe_name);
+}
+
+test {
+    std.testing.refAllDecls(@This());
 }
---
+++ src/qoi.zig
@@ -0,0 +1,141 @@
+const std = @import("std");
+const Vector = std.meta.Vector;
+
+const InputBuf = []align(@alignOf(u32)) const u8;
+
+pub const Metadata = struct {
+    width: u32,
+    height: u32,
+    pixels: usize,
+    channels: enum { rgb, rgba },
+    colorspace: enum { srgb, linear },
+    pixels_start: [*]const u8,
+    pixels_end: [*]const u8,
+
+    pub fn compute(input: InputBuf) error{Invalid}!Metadata {
+        // length of header + end marker
+        if (input.len < 22) return error.Invalid;
+        // magic number
+        if (std.mem.readIntBig(u32, input[0..4]) != 0x716f6966)
+            return error.Invalid;
+        const w = std.mem.readIntBig(u32, input[4..8]);
+        const h = std.mem.readIntBig(u32, input[8..12]);
+        // verify that this doesn't overflow
+        const pixels = std.math.mul(usize, w, h) catch
+            return error.Invalid;
+        // check the last 8 bytes
+        const end = input[input.len - 8 ..];
+        if (std.mem.readIntBig(u64, end[0..8]) != 0x01)
+            return error.Invalid;
+        return Metadata{
+            .width = w,
+            .height = h,
+            .pixels = pixels,
+            .channels = switch (input[12]) {
+                3 => .rgb,
+                4 => .rgba,
+                else => return error.Invalid,
+            },
+            .colorspace = switch (input[13]) {
+                0 => .srgb,
+                1 => .linear,
+                else => return error.Invalid,
+            },
+            .pixels_start = input[14..].ptr,
+            .pixels_end = end.ptr,
+        };
+    }
+};
+
+const Color = Vector(4, u8);
+
+fn hash(col: Color) u6 {
+    const r = col[0];
+    const g = col[1];
+    const b = col[2];
+    const a = col[3];
+    return @truncate(u6, r *% 3 +% g *% 5 +% b *% 7 +% a *% 11);
+}
+
+pub fn decode(input: InputBuf, output: []u32) error{ Invalid, OutputTooSmall }!void {
+    const metadata = try Metadata.compute(input);
+    const input_end = @ptrToInt(metadata.pixels_end);
+    if (output.len < metadata.pixels)
+        return error.OutputTooSmall;
+    var prev_pixels = std.mem.zeroes([64]Vector(4, u8));
+    var col: Vector(4, u8) = .{ 0, 0, 0, 255 };
+    var ptr = metadata.pixels_start;
+    var out: usize = 0;
+    while (@ptrToInt(ptr) < input_end and out < output.len) {
+        const byte = ptr[0];
+        if (byte == 0xfe) {
+            // QOI_OP_RGB
+            col[0] = ptr[1];
+            col[1] = ptr[2];
+            col[2] = ptr[3];
+            prev_pixels[hash(col)] = col;
+            ptr += 4;
+            output[out] = @bitCast(u32, col);
+            out += 1;
+        } else if (byte == 0xff) {
+            // QOI_OP_RGBA
+            col[0] = ptr[1];
+            col[1] = ptr[2];
+            col[2] = ptr[3];
+            col[3] = ptr[4];
+            prev_pixels[hash(col)] = col;
+            ptr += 5;
+            output[out] = @bitCast(u32, col);
+            out += 1;
+        } else switch (@intCast(u2, byte >> 6)) {
+            0b00 => {
+                // QOI_OP_INDEX
+                col = prev_pixels[@truncate(u6, byte)];
+                ptr += 1;
+                output[out] = @bitCast(u32, col);
+                out += 1;
+            },
+            0b01 => {
+                // QOI_OP_DIFF
+                col[0] +%= @bitCast(u8, @as(i8, @bitCast(i2, @truncate(u2, byte >> 4))));
+                col[1] +%= @bitCast(u8, @as(i8, @bitCast(i2, @truncate(u2, byte >> 2))));
+                col[2] +%= @bitCast(u8, @as(i8, @bitCast(i2, @truncate(u2, byte >> 0))));
+                prev_pixels[hash(col)] = col;
+                ptr += 1;
+                output[out] = @bitCast(u32, col);
+                out += 1;
+            },
+            0b10 => {
+                // QOI_OP_LUMA
+                const more_deltas = ptr[1];
+                const dg = @as(i8, @bitCast(i6, @truncate(u6, byte)));
+                const dr = dg + @bitCast(i4, @truncate(u4, more_deltas >> 4));
+                const db = dg + @bitCast(i4, @truncate(u4, more_deltas >> 0));
+                prev_pixels[hash(col)] = col;
+                col[0] +%= @bitCast(u8, dr);
+                col[1] +%= @bitCast(u8, dg);
+                col[2] +%= @bitCast(u8, db);
+                ptr += 2;
+                output[out] = @bitCast(u32, col);
+                out += 1;
+            },
+            0b11 => {
+                // QOI_OP_RUN
+                const repetitions: u8 = @as(u8, @truncate(u6, byte)) + 1;
+                if (out + repetitions > output.len)
+                    return error.Invalid;
+                ptr += 1;
+                std.mem.set(u32, output[out..][0..repetitions], @bitCast(u32, col));
+                out += repetitions;
+            },
+        }
+    }
+    if (@ptrToInt(ptr) != input_end)
+        return error.Invalid;
+    if (out != output.len)
+        return error.Invalid;
+}
+
+test {
+    std.testing.refAllDecls(@This());
+}
